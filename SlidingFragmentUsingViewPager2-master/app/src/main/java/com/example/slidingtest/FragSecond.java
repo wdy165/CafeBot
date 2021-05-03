e7de3a2a6e073c23f701c38dd8906ab33b725080
Soohyun <chltngus@gmail.com>
2021-05-03 22:23:50 +0900

    loading_cafelist

---
+++ SlidingFragmentUsingViewPager2-master/app/src/main/java/com/example/slidingtest/FragSecond.java
@@ -0,0 +1,20 @@
+package com.example.slidingtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FragSecond extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.frame_2p, container, false);
+
+        return rootView;
+    }
+}
---
+++ SlidingFragmentUsingViewPager2-master/app/src/main/java/com/example/slidingtest/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.slidingtest;
+
+import androidx.annotation.NonNull;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+    private CircleIndicator3 mIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(0);
+        mPager.setOffscreenPageLimit(1);
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+
+        });
+    }
+
+}
+
